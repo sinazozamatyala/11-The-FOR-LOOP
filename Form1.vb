56d55d5a03e0a62ff9acfb02717c9ec95d421269
Lenovo-User <Lenovo-User@DESKTOP-JS25F1I>
2024-01-18 22:25:13 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim n As Integer
+        While n <> 5
+            n += 1
+
+            MessageBox.Show(n)
+
+        End While
+        MessageBox.Show("Now out of Loop")
+    End Sub
+End Class
